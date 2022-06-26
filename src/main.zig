861ea59ed52c774463ec328302a9a24341206e7b
Ishan Goel <igoel.mail@gmail.com>
2022-06-25 23:44:22 -0400

    push whatev i hav rn

--- src/main.zig
+++ src/main.zig
@@ -4,6 +4,10 @@ pub fn panic(_: []const u8, _: ?*const std.builtin.StackTrace) noreturn {
     unreachable;
 }
 
+// extern fn print(f: f32) void;
+
+const pixels = @ptrCast(*const [2048][1024][4]u8, @embedFile("../output_binary_RGB8.dat"));
+
 const size = [2]u32{ 500, 500 };
 
 pub const State = extern struct {
@@ -70,28 +74,28 @@ fn norm(v: [3]f32) [3]f32 {
     };
 }
 
-const Quad = struct {
-    pos: [3]f32,
-    norm: [3]f32,
-    tan: [3]f32,
-    size: f32,
-};
+// const Quad = struct {
+//     pos: [3]f32,
+//     norm: [3]f32,
+//     tan: [3]f32,
+//     size: f32,
+// };
 
 const Sphere = struct {
     pos: [3]f32,
     size: f32,
 };
 
-var quad: Quad = .{
-    .pos = [3]f32{ 0, 0, 1 },
-    .norm = [3]f32{ 0, 0, -1 },
-    .tan = [3]f32{ 0, 1, 0 },
-    .size = 0.25,
-};
+// var quad: Quad = .{
+//     .pos = [3]f32{ 0, 0, 1 },
+//     .norm = [3]f32{ 0, 0, -1 },
+//     .tan = [3]f32{ 0, 1, 0 },
+//     .size = 0.25,
+// };
 
 var sphere: Sphere = .{
-    .pos = [3]f32{ 0, 0, 1 },
-    .size = 0.25,
+    .pos = [3]f32{ 0, 0, 2 },
+    .size = 0.5,
 };
 
 export fn draw(dt: f32) void {
@@ -115,9 +119,13 @@ export fn draw(dt: f32) void {
             .direction = lookAt,
         };
 
-        pix[0] = 0;
-        pix[1] = 0;
-        pix[2] = 0;
+        {
+            const color = @floatToInt(u8, 255 * @mod(@round(x / 20) + @round(y / 20), 2));
+            pix[0] = color;
+            pix[1] = color;
+            pix[2] = color;
+            pix[3] = 255;
+        }
 
         // cast ray against quad
 
@@ -144,14 +152,40 @@ export fn draw(dt: f32) void {
         const p = add(look.origin, mulf(look.direction, t0));
 
         const to_p = norm(sub(p, sphere.pos));
-        const light_multip = dot(to_p, norm([3]f32{ @sin(dt * 0.001), 0, @cos(dt * 0.001) }));
+        var light_multip = dot(to_p, norm([3]f32{ @sin(dt * 0.001), 0, @cos(dt * 0.001) }));
+
+        const scatter = 0.2;
+        light_multip = @maximum(light_multip, 0.0) * (1 - scatter) + scatter;
+        // light_multip += std.math.pow(f32, light_multip, 64.0);
+        // light_multip += @exp(light_multip / 20);
+        light_multip = @minimum(light_multip, 1.0);
+
+        // var u = std.math.atan2(f32, p[2], p[0]) / std.math.pi;
+        // var v = std.math.acos(p[1] / sphere.size) / (2 * std.math.pi);
+
+        // var u = std.math.atan2(f32, p[2], p[0]) / (1 * std.math.pi);
+        // var v = std.math.acos(p[1] / sphere.size) / (3 * std.math.pi);
+
+        var u = 0.5 + std.math.atan2(f32, p[0], p[2]) / (2 * std.math.pi);
+        var v = 0.5 + std.math.asin(p[1]) / (3 * std.math.pi);
+
         // var u = dot(quad.tan, to_p);
         // var v = dot(cross(quad.tan, quad.norm), to_p);
         // u = u / sphere.size + 0.5;
         // v = v / sphere.size + 0.5;
 
-        var r = @floatToInt(u8, 255 * @exp(light_multip));
-        var g = @floatToInt(u8, 255 * light_multip);
+        // u += dt * 0.00001;
+        v += dt * 0.00003;
+
+        // print(u);
+        // print(v);
+
+        const pixel_x = @floatToInt(usize, u * 2048);
+        const pixel_y = @floatToInt(usize, v * 1024);
+        const color = @intToFloat(f32, pixels[pixel_x][pixel_y][0]) / 255;
+
+        var r = @floatToInt(u8, color * 255 * light_multip);
+        var g = @floatToInt(u8, color * 255 * light_multip);
         // if (u <= 0 or v <= 0) {
         //     r = 0;
         //     g = 0;
